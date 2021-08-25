7459791f1c4a7c1b7f33b1f554b22cb196fb9315
AhteshamSid <78630600+AhteshamSid@users.noreply.github.com>
2021-08-25 21:02:26 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 if DEBUG:
-    SECRET_KEY = 'django-insecure-grg7^2gn7^pjz6x*1mf*wec5lmjr94l33mj5k=7muh@y#rc#ul'
+    SECRET_KEY = 'SECRET_KEY'
 else:
     SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'signuplogindjango.herokuapp.com']
